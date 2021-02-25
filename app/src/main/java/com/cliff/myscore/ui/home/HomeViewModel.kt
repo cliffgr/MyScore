d1a2ad0fdddcbeb8b1c0cf31877b93f9c4413eb6
kostasantoniou <ka@wappier.com>
2021-02-25 23:45:36 +0200

    Inject HomeViewModel

--- app/src/main/java/com/cliff/myscore/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/cliff/myscore/ui/home/HomeViewModel.kt
@@ -3,11 +3,14 @@ package com.cliff.myscore.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class HomeViewModel : ViewModel() {
+@HiltViewModel
+class HomeViewModel @Inject constructor() : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is home Fragment with Hilt"
     }
     val text: LiveData<String> = _text
 }
