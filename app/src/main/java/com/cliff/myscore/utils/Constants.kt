375210e8084530bb696d4cc21e8ff888f05d7f38
kostasantoniou <ka@wappier.com>
2021-02-25 22:38:45 +0200

    Init Constant object

---
+++ app/src/main/java/com/cliff/myscore/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.cliff.myscore.utils
+
+object Constants {
+    val BASE_URL: String = "https://football.elenasport.io/v2/"
+    val AUTH_URL: String = "https://oauth2.elenasport.io/oauth2/"
+
+}
