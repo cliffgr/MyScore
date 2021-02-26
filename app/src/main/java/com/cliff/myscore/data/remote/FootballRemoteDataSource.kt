d8e7fff57345fe691a20ad66e903f40cba78fa20
kostasantoniou <ka@wappier.com>
2021-02-26 23:14:38 +0200

    added fetchToken method in dataSource

--- app/src/main/java/com/cliff/myscore/data/remote/FootballRemoteDataSource.kt
+++ app/src/main/java/com/cliff/myscore/data/remote/FootballRemoteDataSource.kt
@@ -2,10 +2,21 @@ package com.cliff.myscore.data.remote
 
 import com.cliff.myscore.data.remote.api.AuthApi
 import com.cliff.myscore.data.remote.api.FootballApi
+import com.cliff.myscore.model.TokenRaw
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class FootballRemoteDataSource @Inject constructor(
     private val footballApi: FootballApi,
     private val authApi: AuthApi
 ) {
+
+    suspend fun fetchToken(): Flow<Result<TokenRaw>> {
+        return flow {
+            emit(Result.success(authApi.fetchToken()))
+        }.catch { exception -> emit(Result.failure(exception)) }
+    }
+
 }
