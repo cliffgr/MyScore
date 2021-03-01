c18ba26ca4aca967f6203939425b324e3a4b02b3
kostasantoniou <ka@wappier.com>
2021-03-01 20:26:09 +0200

    Changes in countriesRaw

--- app/src/main/java/com/cliff/myscore/model/CountriesRaw.kt
+++ app/src/main/java/com/cliff/myscore/model/CountriesRaw.kt
@@ -2,20 +2,19 @@ package com.cliff.myscore.model
 
 data class CountriesRaw(
     val errors: List<Any>,
-    val `get`: String,
     val paging: Paging,
     val parameters: List<Any>,
-    val response: List<Response>,
+    val response: List<Country>,
     val results: Int
-) {
-    data class Paging(
-        val current: Int,
-        val total: Int
-    )
+)
 
-    data class Response(
-        val code: String,
-        val flag: String,
-        val name: String
-    )
-}
+data class Paging(
+    val current: Int,
+    val total: Int
+)
+
+data class Country(
+    val code: String,
+    val flag: String,
+    val name: String
+)
