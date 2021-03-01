49a74c373e247aaac281af0f63e871a86ac373b3
kostasantoniou <ka@wappier.com>
2021-03-01 20:26:51 +0200

    Repository will transfer Raw to model

--- app/src/main/java/com/cliff/myscore/data/Repository.kt
+++ app/src/main/java/com/cliff/myscore/data/Repository.kt
@@ -3,8 +3,11 @@ package com.cliff.myscore.data
 import com.cliff.myscore.data.local.FootballLocalDataSource
 import com.cliff.myscore.data.remote.FootballRemoteDataSource
 import com.cliff.myscore.model.CountriesRaw
+import com.cliff.myscore.model.Country
 import dagger.hilt.android.scopes.ViewModelScoped
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 
@@ -14,8 +17,13 @@ class Repository @Inject constructor(
     private val localDataSource: FootballLocalDataSource
 ) {
 
-    suspend fun getCountries(): Flow<Result<CountriesRaw>> {
-        return remoteDataSource.fetchCountries()
+    suspend fun getCountries(): Flow<Result<List<Country>>> {
+        return remoteDataSource.fetchCountries().map {
+            if (it.isSuccess)
+                Result.success(it.getOrNull()!!.response)
+            else
+                Result.failure(it.exceptionOrNull()!!)
+        }
     }
 
 }
