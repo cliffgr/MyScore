ff39f2944c9d2eb8c364c31857dd923a9d9919f7
kostasantoniou <ka@wappier.com>
2021-03-06 08:35:21 +0200

    Added extension function to call a request periodically

--- app/src/main/java/com/cliff/myscore/bl/Mapper.kt
+++ app/src/main/java/com/cliff/myscore/bl/Mapper.kt
@@ -4,6 +4,10 @@ import com.cliff.myscore.model.CountriesRaw
 import com.cliff.myscore.model.Country
 import com.cliff.myscore.model.FixtureLiveScore
 import com.cliff.myscore.model.FixturesRaw
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import javax.inject.Inject
 
 
@@ -13,4 +17,18 @@ fun CountriesRaw.getResponse(): List<Country> {
 
 fun FixturesRaw.getResponse(): List<FixtureLiveScore> {
     return response
+}
+
+fun CoroutineScope.launchPeriodicAsync(
+    repeatMillis: Long,
+    action: () -> Unit
+) = this.async {
+    if (repeatMillis > 0) {
+        while (isActive) {
+            action()
+            delay(repeatMillis)
+        }
+    } else {
+        action()
+    }
 }
