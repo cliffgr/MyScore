9d93c5d60cce2080ee3e249c764ef5fe2ec4f04b
kostasantoniou <ka@wappier.com>
2021-03-23 19:13:29 +0200

    Increased db to v2

--- app/src/main/java/com/cliff/myscore/data/local/AppDatabase.kt
+++ app/src/main/java/com/cliff/myscore/data/local/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.cliff.myscore.data.local.dao.FavouriteLeaguesDao
 import com.cliff.myscore.data.local.entity.FavLeague
 
-@Database(entities = [FavLeague::class], version = 1, exportSchema = true)
+@Database(entities = [FavLeague::class], version = 2, exportSchema = true)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun favouriteLeaguesDao(): FavouriteLeaguesDao
 
