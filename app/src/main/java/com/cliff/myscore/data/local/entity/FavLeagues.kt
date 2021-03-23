4c52477fa79ac26271d29e8a6ac10e9ef7931ed6
kostasantoniou <ka@wappier.com>
2021-03-23 19:17:07 +0200

    Change model values

--- app/src/main/java/com/cliff/myscore/data/local/entity/FavLeagues.kt
+++ app/src/main/java/com/cliff/myscore/data/local/entity/FavLeagues.kt
@@ -4,4 +4,4 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
-data class FavLeague (@PrimaryKey val countryId: Int,val flag : Boolean)
+data class FavLeague (@PrimaryKey val leagueId: Int,val flag : Boolean)
